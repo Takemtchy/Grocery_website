dc9def975198be8764047623951267dfa5fd875c
Takemtchy <KeN4u.gg@gmail.com>
2025-04-10 21:00:54 +0530

    ALL Good

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
-// src/App.jsx
+
+import Cart from './pages/Cart';
+import Login from './pages/Login';
 import MainPage from './pages/MainPage';
 import ProductPage from './pages/ProductPage';
+import SignUp from './pages/Signup';
 
 function App() {
   return (
     <div className="font-sans">
-      <ProductPage />
+      <SignUp />
     </div>
   );
 }
